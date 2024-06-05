b8fd0c1d322f1cd8ec487bde200ff80ce73bc962
Kyle McGee <kyle.mcgee34@gmail.com>
2024-06-04 20:16:53 -0400

    added main.py file

---
+++ main.py
@@ -0,0 +1,13 @@
+import streamlit as st
+import subprocess
+import os
+from analyze import analyze_text
+
+st.title('Text Detector Model')
+text = st.text_area("Text to analyze")
+analyze = st.button('Analyze text')
+if analyze:
+    # Call the function from the other script
+    final_label = analyze_text(text)
+
+    st.write(final_label)
